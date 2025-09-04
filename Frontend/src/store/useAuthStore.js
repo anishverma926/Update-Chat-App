f6522db0770d5816d5a428a3a1b66cfb8c206dd0
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-09-04 20:21:40 +0530

    Update useAuthStore.js

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { data } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://update-chat-app.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
