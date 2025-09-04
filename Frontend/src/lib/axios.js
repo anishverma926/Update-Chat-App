ba882c8cc01dcd99900314766b68bdeab8a0c0c6
Anish <anishverma09072004@gmail.com>
2025-09-04 22:46:32 +0530

    Resolve merge conflicts and clean code

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://update-chat-app.onrender.com/api";
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "/api",
-  withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true, // ensure cookies are sent
 });
